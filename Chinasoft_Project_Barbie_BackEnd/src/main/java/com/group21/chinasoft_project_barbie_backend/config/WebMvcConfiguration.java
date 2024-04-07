bd5a00f576374d16d42c40566c05d3dbfe8672f2
JerreryZzc <1045288412@qq.com>
2024-04-07 10:39:00 +0800

    修改webmvcconfiguration

--- Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/config/WebMvcConfiguration.java
+++ Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/config/WebMvcConfiguration.java
@@ -19,6 +19,6 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry){
         log.info("开始注册自定义拦截器");
         registry.addInterceptor(jwtTokenInterceptor)
-                .addPathPatterns("/user/**");
+                .addPathPatterns("/member/**");
     }
 }
