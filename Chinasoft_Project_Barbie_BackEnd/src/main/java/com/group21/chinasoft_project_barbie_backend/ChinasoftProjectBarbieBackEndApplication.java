57f883d8d96a53bcdb4b50bdb814415f6332dbbf
JerreryZzc <1045288412@qq.com>
2024-04-07 10:31:53 +0800

    1

---
+++ Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/ChinasoftProjectBarbieBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.group21.chinasoft_project_barbie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinasoftProjectBarbieBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinasoftProjectBarbieBackEndApplication.class, args);
+    }
+
+}
