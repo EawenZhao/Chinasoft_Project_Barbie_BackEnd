44e6ae4991bd4680f18335eb6a4f1fa7c3814006
JerreryZzc <1045288412@qq.com>
2024-04-08 14:30:39 +0800

    修改了onMessage的bug

--- Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/ws/ChatEndpoint.java
+++ Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/ws/ChatEndpoint.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@ServerEndpoint(value="/chat/{role}/{id}")
+@ServerEndpoint(value="/chat/{role}")
 @Slf4j
 public class ChatEndpoint {
 
