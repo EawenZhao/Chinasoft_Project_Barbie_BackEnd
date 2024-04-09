f0d3574a946849dead3813305cd40a76b7d7c733
Eawen Chao <zyh1999zyh@yeah.net>
2024-04-09 20:23:41 +0800

    Disable JWT

--- Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/config/WebMvcConfiguration.java
+++ Chinasoft_Project_Barbie_BackEnd/src/main/java/com/group21/chinasoft_project_barbie_backend/config/WebMvcConfiguration.java
@@ -19,11 +19,10 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      */
     protected void addInterceptors(InterceptorRegistry registry){
         log.info("开始注册自定义拦截器");
-        registry.addInterceptor(jwtTokenInterceptor)
-                .addPathPatterns("/member/**")
-                .excludePathPatterns("/member/login")
-                .excludePathPatterns("/member/register")
-                .addPathPatterns("/exception_elimination");
+//        registry.addInterceptor(jwtTokenInterceptor)
+//                .addPathPatterns("/member/**")
+//                .excludePathPatterns("/member/login")
+//                .excludePathPatterns("/member/register");
     }
 
     @Override
